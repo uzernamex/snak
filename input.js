49cd01bff9244556e7ae78e28f851aa07fce1eef
Ayesha Cumberbatch <ayesha.416x@gmail.com>
2023-05-27 23:01:43 -0400

    code added to constants

--- input.js
+++ input.js
@@ -1,20 +1,27 @@
 let connection;
-
-const setupInput = (conn) => {
+const setupInput = function(conn) {
   connection = conn;
-//};
-//const setupInput = function () {
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
+  stdin.on("data", handleUserInput);
   return stdin;
 };
+module.exports = { setupInput };
 
 
-stdin.on("data", handleUserInput);
-const handleUserInput = function () {
-  // your code here
+const handleUserInput = function(data) {
+  if (data === "w") {
+    connection.write("Move up");
+  } if (data === "a") {
+    connection.write("Move left");
+  } if (data === "s") {
+    connection.write("Move down");
+  } if (data === "d") {
+    connection.write("Move right");
+  } if (data === "ctrl+C") {
+    process.exit();
+  }
 };
-
-module.exports = {setupInput}
+module.exports = { handleUserInput };
