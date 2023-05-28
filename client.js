9a915028ece1687b133b41ad2c12f99a68c7a022
Ayesha Cumberbatch <ayesha.416x@gmail.com>
2023-05-27 23:03:06 -0400

    updated code

--- client.js
+++ client.js
@@ -1,81 +1,16 @@
-// server.js
-const { IP, PORT } = require("./constants");
-const net = require("net");
-const server = net.createServer();
-server.on("connection", (client) => {
-  console.log("New client connected!");
-
-  client.write("Hello from the server!");
-
-  client.setEncoding("utf8"); // interpret data as text
-
-  client.on("data", (data) => {
-    console.log("Message from client: ", data);
-
-  client.write("Name: ___")
-    
-
-  });
-});
-server.listen(3000, () => {
-  console.log("Server listening on port 3000!");
-});
-// client.js
-const net = require("net");
-const conn = net.createConnection({
-  host: "localhost",
-  port: 5500
-});
-conn.on("data", (data) => {
-  console.log("Server says: ", data);
-});
-conn.on("connect", () => {
-  conn.write("Hello from client!");
-});
-conn.setEncoding("utf8");
-
-//conn.on("move up", (data) => {
-//  console.log("Server says: ", data);
-//});
-
-/*
-const connect = function () {
+const connect = function() {
   const conn = net.createConnection({
     host: "localhost",
-    port: 5500,
-   })
-    
-    conn.on("connect", () => {
-      console.log('Successfully connected to game server');
-      return "Name: ___"
-    });
-    
-
-
-conn.on("connect", () => {
-  conn.write("Hello from client!");
-});
-
-
-
-
-  // interpret incoming data as text
+    port: 50541
+  });
   conn.setEncoding("utf8");
-  conn.addListener; {
-  console.log (conn)
-  };
-
-
-  return conn;
+  conn.on("connect", (server) => {
+    console.log("Successfully connected to the game server");
+    server.write("Name: MAC");
+    return conn;
+  });
 };
-
-client.setEncoding("utf8"); // interpret data as text
-client.on("data", (data) => {
-  console.log("Message from client: ", data);
-});
-
 console.log("Connecting ...");
 connect();
 
-module.exports = {connect}
-*/
+module.exports = { connect };
--- constants.js
+++ constants.js
@@ -2,3 +2,19 @@ module.exports = {
   IP: "Localhost",
   PORT: 5500,
 };
+
+module.exports = {
+  handleUserInput = function(data) {
+    if (data === "w") {
+      connection.write("Move up")
+    } if (data === "a") {
+      connection.write("Move left")
+    } if (data === "s") {
+      connection.write("Move down");
+    } if (data === "d") {
+      connection.write("Move right");
+    } if (data === "ctrl+C") {
+      process.exit();
+    }
+  }
+};
